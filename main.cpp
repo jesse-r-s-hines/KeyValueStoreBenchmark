5a2b1d51ca03c4403da590326410470f00e88482
Jesse Hines <jessehines@southern.edu>
2022-02-13 15:57:41 -0500

    Update main.cpp for sqlite

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <filesystem>
+#include <random>
 
 #include <SQLiteCpp/SQLiteCpp.h>
 #include "rocksdb/db.h"
@@ -8,12 +9,24 @@
 
 #include "dbwrappers.cpp"
 
-using std::exception, std::runtime_error, std::string;
+using std::exception, std::runtime_error, std::string, std::size_t;
 
-void test_sqlite3() {
+std::random_device rd;
+std::mt19937 randGen(rd());
+
+string randBlob(size_t size) {
+    std::uniform_int_distribution<unsigned char> randChar(0, 10);
+    string blob(size, '\0');
+    for (size_t i = 0; i < size; i++) {
+        blob[i] = randChar(randGen);
+    }
+    return blob;
+}
+
+void testSqlite() {
     dbwrappers::SQLiteWrapper db("dbs/sqlite3.db");
 
-    db.insert("key", "hello");
+    db.insert("key", randBlob(10));
     db.update("key", "hello world");
     std::cout << db.get("key") << "\n";
     db.remove("key");
@@ -26,44 +39,44 @@ void test_sqlite3() {
     }
 }
 
-void run_rocksdb() {
-    rocksdb::DB* db;
-    std::cout << "RocksDB version: " << rocksdb::GetRocksVersionAsString() << std::endl;
+// void run_rocksdb() {
+//     rocksdb::DB* db;
+//     std::cout << "RocksDB version: " << rocksdb::GetRocksVersionAsString() << std::endl;
 
-    rocksdb::Options options;
-    options.create_if_missing = true;
+//     rocksdb::Options options;
+//     options.create_if_missing = true;
 
-    rocksdb::Status status = rocksdb::DB::Open(options, "dbs/rocksdb.db", &db);
-    std::cout << "RocksDB status: " << (status.ok()) << std::endl;
+//     rocksdb::Status status = rocksdb::DB::Open(options, "dbs/rocksdb.db", &db);
+//     std::cout << "RocksDB status: " << (status.ok()) << std::endl;
 
-    // status = db->Put(rocksdb::WriteOptions(), "key", "value");
-}
+//     // status = db->Put(rocksdb::WriteOptions(), "key", "value");
+// }
 
-void run_leveldb() {
-    leveldb::DB* db;
-    std::cout << "leveldb version: " << leveldb::kMajorVersion << "." << leveldb::kMinorVersion << std::endl;
+// void run_leveldb() {
+//     leveldb::DB* db;
+//     std::cout << "leveldb version: " << leveldb::kMajorVersion << "." << leveldb::kMinorVersion << std::endl;
 
-    leveldb::Options options;
-    options.create_if_missing = true;
+//     leveldb::Options options;
+//     options.create_if_missing = true;
 
-    leveldb::Status status = leveldb::DB::Open(options, "dbs/leveldb.db", &db);
-    std::cout << "leveldb status: " << (status.ok()) << std::endl;
-}
+//     leveldb::Status status = leveldb::DB::Open(options, "dbs/leveldb.db", &db);
+//     std::cout << "leveldb status: " << (status.ok()) << std::endl;
+// }
 
-void run_berkeleydb() {
-    std::cout << "Berkeley version: " << DB_VERSION_STRING << std::endl;
-    Db db(NULL, 0); // Instantiate the Db object
-    u_int32_t oFlags = DB_CREATE; // Open flags;
-    // Open the database
-    int status = db.open(NULL, "dbs/berkeleydb.db", NULL, DB_BTREE, oFlags, 0);
-    std::cout << "Berkeleydb status: " << status << "\n";
-}
+// void run_berkeleydb() {
+//     std::cout << "Berkeley version: " << DB_VERSION_STRING << std::endl;
+//     Db db(NULL, 0); // Instantiate the Db object
+//     u_int32_t oFlags = DB_CREATE; // Open flags;
+//     // Open the database
+//     int status = db.open(NULL, "dbs/berkeleydb.db", NULL, DB_BTREE, oFlags, 0);
+//     std::cout << "Berkeleydb status: " << status << "\n";
+// }
 
 int main() {
     std::filesystem::remove_all("dbs");
     std::filesystem::create_directory("dbs");
 
-    test_sqlite3();
+    testSqlite();
     // std::cout << std::endl;
     // run_rocksdb();
     // std::cout << std::endl;
