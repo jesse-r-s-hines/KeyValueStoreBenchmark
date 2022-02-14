0a1bc75d2de862d2d00dc0ec0178114358a34b3a
Jesse Hines <jessehines@southern.edu>
2022-02-14 14:23:31 -0500

    Basic benchmark

--- src/helpers.h
+++ src/helpers.h
@@ -2,10 +2,28 @@
 #include <random>
 #include <functional>
 #include <chrono>
+#include<iostream>
 
 namespace helpers {
     std::random_device randomDevice;
     std::mt19937 randGen(randomDevice());
+
+    template< typename T >
+    std::string intToHex(T i, int width) {
+        std::stringstream stream;
+        stream << std::setfill('0') << std::setw(width) << std::hex << i;
+        return stream.str();
+    }
+
+    template< typename T >
+    std::string intToHex(T i) {
+        return intToHex(i, sizeof(T)*2);
+    }
+
+    std::string randHash() {
+        std::uniform_int_distribution<long long unsigned> randNum(0, 0xFFFF'FFFF'FFFF'FFFF);
+        return intToHex(randNum(randGen), 16);
+    }
 
     std::string randBlob(size_t size) {
         std::uniform_int_distribution<unsigned char> randChar(0, 10);
@@ -16,7 +34,7 @@ namespace helpers {
         return blob;
     }
 
-    std::chrono::nanoseconds timeFunc(std::function<void()> func) {
+    std::chrono::nanoseconds timeIt(std::function<void()> func) {
         auto start = std::chrono::steady_clock::now();
         func();
         auto stop = std::chrono::steady_clock::now();
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 #include <random>
 #include <memory>
 #include <chrono>
+#include <vector>
+#include <map>
+#include <tuple>
+#include <cmath>
 
 #include "dbwrappers.h"
 #include "helpers.h"
@@ -11,10 +15,97 @@
 #include "doctest/doctest.h"
 #include "tests.cpp"
 
+using std::vector, std::map, std::tuple, std::string, std::unique_ptr, std::make_unique;
+
+template<typename T>
+class Stats {
+    long long _count = 0;
+    T _sum;
+    T _min;
+    T _max;
+
+public:
+    void record(T value) {
+        _sum += value;
+        if (_count == 0 || value < _min) _min = value;
+        if (_count == 0 || value > _max) _max = value;
+        _count++;
+    }
+
+    /** Note: Divide by zero if we haven't recording anything */
+    T count() { return _count; }
+    T sum() { return _sum; }
+    T min() { return _min; }
+    T max() { return _max; }
+    T avg() { return _sum / _count; }
+};
+
+class BenchmarkData {
+public:
+    map<tuple<string, string>, Stats<long long>> data;
+    void record(string db, string operation, long long value) {
+        data[{db, operation}].record(value);
+    }
+
+    Stats<long long> get(string db, string operation) {
+        return data[{db, operation}];
+    }
+};
+
+
+BenchmarkData runBenchmark() {
+    std::filesystem::remove_all("dbs");
+    std::filesystem::create_directories("dbs");
+
+    vector<unique_ptr<dbwrappers::DBWrapper>> dbs{}; // can't use initializer list with unique_ptr for some reason
+    dbs.push_back(make_unique<dbwrappers::SQLiteWrapper>("dbs/sqlite3.db"));
+    dbs.push_back(make_unique<dbwrappers::LevelDBWrapper>("dbs/leveldb.db"));
+    dbs.push_back(make_unique<dbwrappers::RocksDBWrapper>("dbs/rocksdb.db"));
+    dbs.push_back(make_unique<dbwrappers::BerkeleyDBWrapper>("dbs/berkleydb.db"));
+
+    BenchmarkData data;
+
+    for (auto& db : dbs) {
+        for (int i = 0; i < 100; i++) {
+            string key = helpers::randHash();
+            string blob = helpers::randBlob(1024);
+
+            auto time = helpers::timeIt([&]() { db->insert(key, blob); });
+            data.record(db->type(), "insert", time.count());
+
+            blob = helpers::randBlob(1024);
+            time = helpers::timeIt([&]() { db->update(key, blob); });
+            data.record(db->type(), "update", time.count());
+
+            string value;
+            time = helpers::timeIt([&]() {
+                value = db->get(key);
+            });
+            if (value.size() == 0) throw std::runtime_error("DB get failed"); // make sure compiler doe
+            data.record(db->type(), "get", time.count());
+
+            time = helpers::timeIt([&]() { db->remove(key); });
+            data.record(db->type(), "remove", time.count());
+        }
+    }
+
+    return data;
+}
+
+
+
 int main(int argc, char** argv) {
     doctest::Context context;
     context.setOption("minimal", true); // only show if errors occur.
     context.applyCommandLine(argc, argv);
     int res = context.run();
     if(context.shouldExit()) return res;
+
+    BenchmarkData data = runBenchmark();
+
+    for (auto& record : data.data) {
+        std::cout << "[";
+        std::apply([](auto&&... t) { ((std::cout << t << ", "), ...); }, record.first);
+        std::cout << "] : " << record.second.avg() << "ns" << std::endl;
+    }
 }
