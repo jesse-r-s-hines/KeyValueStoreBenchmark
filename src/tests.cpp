d8785fe2f660c8b352301c75f9821b487a0c57bc
Jesse Hines <jessehines@southern.edu>
2022-02-20 08:02:10 -0500

    Tweaks to tests
    Use random key of fixed size

--- src/tests.cpp
+++ src/tests.cpp
@@ -7,6 +7,7 @@
 #include "doctest/doctest.h"
 
 #include "stores.h"
+#include "utils.h"
 
 namespace tests {
     using std::unique_ptr, std::make_unique, std::vector, std::array, std::pair, std::string;
@@ -24,28 +25,30 @@ namespace tests {
         SUBCASE("Basic") {
             for (auto& store : stores) {
                 INFO(store->typeName());
+                string key = utils::randHash(32); 
 
-                store->insert("key", "value");
-                REQUIRE(store->get("key") == "value");
+                store->insert(key, "value");
+                REQUIRE(store->get(key) == "value");
 
-                store->update("key", "updated");
-                REQUIRE(store->get("key") == "updated");
+                store->update(key, "updated");
+                REQUIRE(store->get(key) == "updated");
 
-                store->remove("key");
+                store->remove(key);
 
-                REQUIRE_THROWS(store->get("key"));
+                REQUIRE_THROWS(store->get(key));
             }
         }
 
         SUBCASE("Nulls") {
             for (auto& store : stores) {
                 INFO(store->typeName());
+                string key = utils::randHash(32); 
 
-                store->insert("key", "hello\0world"s);
-                REQUIRE(store->get("key") == "hello\0world"s);
+                store->insert(key, "hello\0world"s);
+                REQUIRE(store->get(key) == "hello\0world"s);
 
-                store->update("key", "\0goodbye\0"s);
-                REQUIRE(store->get("key") == "\0goodbye\0"s);
+                store->update(key, "\0goodbye\0"s);
+                REQUIRE(store->get(key) == "\0goodbye\0"s);
             }
         }
     }
@@ -58,22 +61,45 @@ namespace tests {
         for (auto [type, typeName] : stores::types) {
             string filepath = "out/tests/"s + typeName;
             INFO(filepath);
+            string key = utils::randHash(32); 
 
             REQUIRE(!filesystem::exists(filepath));
             { // create the store, and then close it.
                 auto store = stores::getStore(type, filepath, true);
-                store->insert("key", "value");
+                REQUIRE(store->filepath == filepath);
+
+                store->insert(key, "value");
             }
             REQUIRE(filesystem::exists(filepath));
 
             {
                 auto store = stores::getStore(type, filepath, false);
-                REQUIRE(store->get("key") == "value");
+                REQUIRE(store->get(key) == "value");
             }
 
             {
                 auto store = stores::getStore(type, filepath, true);
-                REQUIRE_THROWS(store->get("key")); // We wiped the database
+                REQUIRE_THROWS(store->get(key)); // We wiped the database
+            }
+        }
+    }
+
+    TEST_CASE("Test multiple records") {
+        // Make a bunch of records. Run this last so can examine the db manually as well.
+        filesystem::remove_all("out/tests");
+        filesystem::create_directories("out/tests/");
+
+        vector<unique_ptr<stores::Store>> stores{};
+        for (auto [type, typeName] : stores::types)
+            stores.push_back(stores::getStore(type, "out/tests/"s + typeName, true));
+
+        for (auto& store : stores) {
+            for (int i = 0; i < 25; i++) {
+                INFO(store->typeName());
+                string key = utils::randHash(32);
+                string value = utils::randBlob(64);
+                store->insert(key, value);
+                REQUIRE(store->get(key) == value);
             }
         }
     }
