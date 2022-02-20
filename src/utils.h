448ccd78b501c549868313854b281012051d6507
Jesse Hines <jessehines@southern.edu>
2022-02-20 10:35:02 -0500

    Add some utils

--- src/utils.h
+++ src/utils.h
@@ -10,6 +10,15 @@
 namespace utils {
     std::string intToHex(long long i, int width);
 
+    /** Converts a boost digest into a hex string */
+    template<typename HashDigest>
+    std::string digestToString(HashDigest digest) {
+        std::stringstream ss;
+        for (auto part : digest)
+            ss << std::setfill('0') << std::setw(sizeof(part) * 2) << std::hex << part;
+        return ss.str();
+    };
+
     /** Random int in range (inclusive) */
     int randInt(int min, int max);
 
@@ -21,9 +30,6 @@ namespace utils {
 
     long long diskUsage(const std::filesystem::path& filepath);
 
-    template<typename T, typename... Ts>
-    using AllSame = std::enable_if_t<std::conjunction_v<std::is_same<T, Ts>...>>;
-
     /**
      * Returns a new map that contains values of all the given maps.
      */
@@ -32,7 +38,10 @@ namespace utils {
         MapLike rtrn = first;
         (std::for_each(rest.begin(), rest.end(), [&](auto& pair) { rtrn[pair.first] = pair.second; }), ...);
         return rtrn;
-    }
+    };
+
+    template<typename T>
+    struct Range { T min; T max; };
 
     /** Keeps the average and other statistics. */
     template<typename T>
