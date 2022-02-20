f5647139eda71a832443a9772de2591981798d08
Jesse Hines <jessehines@southern.edu>
2022-02-20 08:02:30 -0500

    Modification to merge

--- src/utils.h
+++ src/utils.h
@@ -4,6 +4,7 @@
 #include <filesystem>
 #include <chrono>
 #include <map>
+#include <algorithm>
 #include <type_traits>
 
 namespace utils {
@@ -20,18 +21,13 @@ namespace utils {
     template<typename T, typename... Ts>
     using AllSame = std::enable_if_t<std::conjunction_v<std::is_same<T, Ts>...>>;
 
-    template<typename K, typename V>
-    std::map<K, V> merge(const std::map<K, V>& first) {
-        return first;
-    }
-
     /**
      * Returns a new map that contains values of all the given maps.
      */
-    template<typename K, typename V, typename... Maps, typename = AllSame<std::map<K, V>, Maps...>>
-    std::map<K, V> merge(const std::map<K, V>& first, const Maps&... rest) {
-        std::map<K, V> rtrn = first;
-        for (auto& [key, value] : merge(rest...)) rtrn[key] = value;
+    template<typename MapLike, typename... MapLikes>
+    MapLike merge(const MapLike first, const MapLikes&... rest) {
+        MapLike rtrn = first;
+        (std::for_each(rest.begin(), rest.end(), [&](auto& pair) { rtrn[pair.first] = pair.second; }), ...);
         return rtrn;
     }
 
